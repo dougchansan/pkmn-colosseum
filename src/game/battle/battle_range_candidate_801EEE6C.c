598b5e78c3b084356fc43735a677adcbdd703ca8
dougchansan <douglasw808@gmail.com>
2026-07-19 13:57:13 -1000

    Test standalone fn_801EEE6C

--- src/game/battle/battle_range_candidate_801EEE6C.c
+++ src/game/battle/battle_range_candidate_801EEE6C.c
@@ -1,2 +1,12 @@
-#define BATTLE_RANGE_CANDIDATE_801EE958
-#include "src/game/battle/battle_range_801ED640.c"
+#include "dolphin/types.h"
+
+extern u8* fn_801EF1E4(u32 data);
+
+void fn_801EEE6C(u16 id, u16 value)
+{
+    u8* data = fn_801EF1E4(0);
+
+    if (data != NULL) {
+        *(u16*)(data + id * 0x18 + 0x4A6) = value;
+    }
+}
