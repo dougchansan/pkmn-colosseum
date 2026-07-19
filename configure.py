01e38bede0975bcd5d75c0b7ada655021ed9ac89
dougchansan <dougchansan@users.noreply.github.com>
2026-07-18 17:00:13 -1000

    Link exact GS model bound prefix

--- configure.py
+++ configure.py
@@ -1930,7 +1930,7 @@ config.libs = [
                 progress_category="game",
             ),
             Object(
-                CodeCandidate,
+                Matching,
                 "game/gs_model_bound.c",
                 mw_version="GC/1.3",
                 extra_cflags=["-use_lmw_stmw on", "-sdata 8", "-sdata2 8"],
