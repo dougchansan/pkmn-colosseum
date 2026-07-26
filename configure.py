91a3c2a84de5bb9c5a4d6d4999733bb315f99ca7
dougchansan <dougchansan@users.noreply.github.com>
2026-07-26 00:00:06 -1000

    Use HSD relation register block saves

--- configure.py
+++ configure.py
@@ -3539,7 +3539,7 @@ config.libs = [
                 CodeCandidate,
                 "hsd/hsd_robj_range_801AEBE4.c",
                 mw_version="GC/1.3",
-                extra_cflags=["-O2"],
+                extra_cflags=["-O2", "-use_lmw_stmw on"],
                 progress_category="hsd",
             ),
             Object(
