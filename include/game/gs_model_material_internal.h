0d64c1dd4a0056f10b231c56221b71eeb1abf65b
dougchansan <dougchansan@users.noreply.github.com>
2026-07-25 22:59:31 -1000

    Refine GS model material acquisition

--- include/game/gs_model_material_internal.h
+++ include/game/gs_model_material_internal.h
@@ -15,46 +15,43 @@ static inline u16 GSmodelAcquireMaterials(GSmodel* model)
     s32 partCount;
     s32 partIndex;
 
-    if (model->modulationRefCount != 0) {
-        model->modulationRefCount++;
-        return model->materialCount;
-    }
+    if (model->modulationRefCount == 0) {
+        count = 0;
+        partCount = fn_800EE0E8(model);
+        for (partIndex = 0; partIndex < partCount; partIndex++) {
+            void* part = fn_800EE150(model, partIndex);
+            count += fn_800EE758(part);
+            fn_800EE828(part);
+        }
 
-    count = 0;
-    partCount = fn_800EE0E8(model);
-    for (partIndex = 0; partIndex < partCount; partIndex++) {
-        void* part = fn_800EE150(model, partIndex);
-        count += fn_800EE758(part);
-        fn_800EE828(part);
-    }
+        if (count != 0) {
+            u16 handle = fn_800E3534(count * sizeof(void*));
 
-    if (count != 0) {
-        u16 handle = fn_800E3534(count * sizeof(void*));
+            if (handle != 0) {
+                void** dst = fn_800E27B0(handle);
 
-        if (handle != 0) {
-            void** dst = fn_800E27B0(handle);
+                model->materialList = (GSmodelMaterialList*)dst;
+                model->materialListHandle = handle;
+                model->materialCount = count;
+                for (partIndex = 0; partIndex < partCount; partIndex++) {
+                    void* part = fn_800EE150(model, partIndex);
+                    s32 partMaterialCount = fn_800EE758(part);
+                    s32 materialIndex;
 
-            model->materialList = (GSmodelMaterialList*)dst;
-            model->materialListHandle = handle;
-            model->materialCount = count;
-            for (partIndex = 0; partIndex < partCount; partIndex++) {
-                void* part = fn_800EE150(model, partIndex);
-                s32 partMaterialCount = fn_800EE758(part);
-                s32 materialIndex;
-
-                for (materialIndex = 0; materialIndex < partMaterialCount;
-                     materialIndex++) {
-                    *dst++ = fn_800EE6B4(part, materialIndex);
+                    for (materialIndex = 0; materialIndex < partMaterialCount;
+                         materialIndex++) {
+                        *dst++ = fn_800EE6B4(part, materialIndex);
+                    }
+                    fn_800EE828(part);
                 }
-                fn_800EE828(part);
+            } else {
+                count = 0;
             }
-        } else {
-            count = 0;
         }
     }
 
     model->modulationRefCount++;
-    return count;
+    return model->materialCount;
 }
 
 #endif
