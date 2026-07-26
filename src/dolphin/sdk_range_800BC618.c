75aab1d950de1af7e05f52fbf36bdb2c2953151d
dougchansan <dougchansan@users.noreply.github.com>
2026-07-26 03:57:50 -1000

    Use canonical GXSetTevOrder field expressions

--- src/dolphin/sdk_range_800BC618.c
+++ src/dolphin/sdk_range_800BC618.c
@@ -11,8 +11,6 @@ void fn_800BC6F0(u32 stage, u32 coord, u32 map, u32 color)
     u32* ptref;
     u32 tmap;
     u32 tcoord;
-    u32 raster;
-    u32 enabled;
 
     ptref = &gx->tref[stage / 2];
     gx->texmapId[stage] = map;
@@ -30,18 +28,20 @@ void fn_800BC6F0(u32 stage, u32 coord, u32 map, u32 color)
         gx->tevTcEnab |= 1 << stage;
     }
 
-    raster = color == 0xFF ? 7 : lbl_803135E0[color];
-    enabled = map != 0xFF && (map & 0x100) == 0;
     if (stage & 1) {
         *ptref = (*ptref & ~0x7000U) | (tmap << 12);
         *ptref = (*ptref & ~0x38000U) | (tcoord << 15);
-        *ptref = (*ptref & ~0x380000U) | (raster << 19);
-        *ptref = (*ptref & ~0x40000U) | (enabled << 18);
+        *ptref = (*ptref & ~0x380000U) |
+                 ((color == 0xFF ? 7 : lbl_803135E0[color]) << 19);
+        *ptref = (*ptref & ~0x40000U) |
+                 ((map != 0xFF && (map & 0x100) == 0) << 18);
     } else {
         *ptref = (*ptref & ~0x7U) | tmap;
         *ptref = (*ptref & ~0x38U) | (tcoord << 3);
-        *ptref = (*ptref & ~0x380U) | (raster << 7);
-        *ptref = (*ptref & ~0x40U) | (enabled << 6);
+        *ptref = (*ptref & ~0x380U) |
+                 ((color == 0xFF ? 7 : lbl_803135E0[color]) << 7);
+        *ptref = (*ptref & ~0x40U) |
+                 ((map != 0xFF && (map & 0x100) == 0) << 6);
     }
 
     GX_BP_REG(*ptref);
