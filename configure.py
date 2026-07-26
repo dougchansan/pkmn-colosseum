e2852472272d7037aa7a45bc1cd30a26ace2f4be
dougchansan <dougchansan@users.noreply.github.com>
2026-07-26 00:02:10 -1000

    Use HSD joint register block saves

--- configure.py
+++ configure.py
@@ -6849,6 +6849,7 @@ config.libs = [
                 CodeCandidate,
                 "hsd/hsd_jobj_residual_801A0D94.c",
                 mw_version="GC/1.3",
+                extra_cflags=["-use_lmw_stmw on"],
                 progress_category="hsd",
             ),
             Object(
