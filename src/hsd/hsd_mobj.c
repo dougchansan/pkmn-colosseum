2b5613a205dcff22800b4dce8eaaa9c16ff043f9
dougchansan <dougchansan@users.noreply.github.com>
2026-07-25 23:34:55 -1000

    Match HSD texture prepend guard

--- src/hsd/hsd_mobj.c
+++ src/hsd/hsd_mobj.c
@@ -215,10 +215,7 @@ void HSD_MObjAddTObjNext(HSD_MObj* mobj, HSD_TObj* tobj, HSD_TObj* next)
 /* 0x801A6DA0 | 0x24 */
 void fn_801A6DA0(HSD_MObj* mobj, HSD_TObj* tobj)
 {
-    if (mobj == NULL) {
-        return;
-    }
-    if (tobj == NULL) {
+    if (mobj == NULL || tobj == NULL) {
         return;
     }
     tobj->next = mobj->tobj;
