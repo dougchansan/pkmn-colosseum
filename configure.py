900da6472e7f5c059583d00a7b487ed6e5897c8e
dougchansan <dougchansan@users.noreply.github.com>
2026-07-26 02:20:55 -1000

    Use matching compiler for HSD AObj animation

--- configure.py
+++ configure.py
@@ -7216,7 +7216,7 @@ config.libs = [
             Object(
                 CodeCandidate,
                 "hsd/hsd_aobj_candidate_801C028C.c",
-                mw_version="GC/1.3",
+                mw_version="GC/1.3.2",
                 extra_cflags=[
                     "-use_lmw_stmw on",
                     "-sdata 8",
