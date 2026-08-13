89aad53f3c65ea6d290173218a32a19c71d3fad5
dougchansan <douglasw808@gmail.com>
2026-08-13 06:32:24 -1000

    Reconnect NPC event state five
    The state dispatcher now invokes the retail transition helper for state five instead of falling into the generic tail.\n\nConfidence: high\nScope-risk: narrow\nTested: ninja -j4 build/GC6E01/report.json; fn_80031B4C fuzzy 59.79% to 60.46%\nNot-tested: Interactive NPC exchange flow

--- src/game/gs_npc_event.c
+++ src/game/gs_npc_event.c
@@ -755,6 +755,9 @@ void fn_80031B4C(void) {
                     fadeCheck(1);
                     lbl_8047A42C = 2;
                     goto L_800323E4;
+                case 5:
+                    fn_8002EA5C();
+                    goto L_800323E4;
                 case 10:
                     fadeSet(3, lbl_8047B9D0);
                     fadeCheck(1);
